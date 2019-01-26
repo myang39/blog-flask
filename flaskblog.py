317a492b5b7388bbc478cdb80c553aadde4acbf4
Minkang Yang <minkangyang@gmail.com>
2019-01-25 23:37:05 -0500

    add templates test with dummy posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Luke Yang',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018' 
+    },
+    {
+        'author': 'Marry Wang',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home Page<h1>"
 
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':

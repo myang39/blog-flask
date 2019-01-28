4068eeffa56b55afc7e4234bb880bacbbaa54057
Minkang Yang <minkangyang@gmail.com>
2019-01-27 21:09:23 -0500

    validate username and email for uniqueness

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'eb4cdc7ecf5a37328813c59cd09f7e93'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from flaskblog import routes
 
